dcadc5008f3122d1a9f525ff2f9814ea1583a090
mariog44 <mg441236@gmail.com>
2024-08-16 15:38:20 -0500

    PROYECTO BASE. Se creo el proyecto base para el proyecto de las referencias de los repuestos de motocicletas.

---
+++ src/main/java/ReferencesProject/ReferencesProjectApplication.java
@@ -0,0 +1,13 @@
+package ReferencesProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferencesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferencesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ReferencesProject/ReferencesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ReferencesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferencesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
